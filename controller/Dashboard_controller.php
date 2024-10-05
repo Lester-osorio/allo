e653db0e7950310b13c4f9c022f1ccb4375d0a9b
Lester-osorio <147952107+Lester-osorio@users.noreply.github.com>
2024-10-05 10:30:37 -0600

    Add files via upload

---
+++ controller/Dashboard_controller.php
@@ -0,0 +1,48 @@
+<?php
+
+class Dashboard
+{
+
+	public function Ver_Moneda_Reporte()
+	{
+
+		$filas = DashboardModel::Ver_Moneda_Reporte();
+		return $filas;
+	}
+
+
+	public function Datos_Paneles()
+	{
+
+		$filas = DashboardModel::Datos_Paneles();
+		return $filas;
+	}
+
+	public function Compras_Anuales()
+	{
+
+		$filas = DashboardModel::Compras_Anuales();
+		return $filas;
+	}
+
+	public function Ventas_Anuales()
+	{
+
+		$filas = DashboardModel::Ventas_Anuales();
+		return $filas;
+	}
+
+	public function CantidadProductosStockBajo()
+	{
+
+		$filas = DashboardModel::CantidadProductosStockBajo();
+		return $filas;
+	}
+
+	public function CalcularGananciaNetaTotal()
+	{
+
+		$filas = DashboardModel::CalcularGananciaNetaTotal();
+		return $filas;
+	}
+}
