75a4cc478062d3cfeceb1cde13ddddbba8c3acb7
Lester-osorio <147952107+Lester-osorio@users.noreply.github.com>
2024-10-05 10:54:35 -0600

    Add files via upload

---
+++ view/info.vw.php
@@ -0,0 +1,193 @@
+<!-- Content area -->
+<div class="content">
+
+  <div class="row">
+    <div class="col-md-12">
+
+      <!-- Widget with rounded icon -->
+      <div class="panel">
+        <div class="panel-body text-center">
+          <!--           <div class="icon-object border-primary-400 text-primary-400"><i class="icon-info3 icon-3x text-primary-400"></i>
+          </div> -->
+
+          <h2 class="no-margin text-semibold">Misión </h2><br>
+          <span class="text-size-large text-muted">Ser una llantera líder en Productos y Servicios de Calidad, brindando Confianza, Seguridad
+            y garantía en nuestros productos y servicios, siempre innovando la
+            tecnología por la que nos hemos caracterizado en nuestros talleres, y así poder servirles con rapidez y responsabilidad en
+            todos los trabajos a realizarse. </span> <br><br>
+
+
+          <h2 class="no-margin text-semibold">Visión </h2><br>
+          <span class="text-size-large text-muted">Tenemos como finalidad la satisfacción total de nuestros clientes, por lo que buscamos superar sus expectativas
+            otorgando productos y servicios que satisfagan sus necesidades. Para esto ofrecemos productos de la más alta calidad, y nos encontramos en constante
+            evaluación, buscando la mejora continua de nuestros procesos y nuestro personal, para de esta manera ser la llantera más
+            confiable y de mejor calidad del Occidente. </span> <br><br>
+
+
+
+        </div>
+      </div>
+      <!-- /widget with rounded icon -->
+    </div>
+  </div>
+
+  <!-- Widgets list -->
+  <div class="row">
+    <div class="panel-body text-center">
+
+      <h2 class="no-margin text-semibold">Tecnologías Usadas </h2><br>
+
+    </div>
+    <div class="col-sm-6 col-lg-3">
+      <!-- Simple stats with thumbnail -->
+      <div class="panel">
+        <a href="#">
+          <img src="web/assets/images/php.png" class="img-responsive" alt="">
+        </a>
+        <div class="panel-body">
+          <div class="row text-center">
+            <div class="col-xs-4">
+            </div>
+            <div class="col-xs-4">
+              <h5 class="text-semibold no-margin">PHP</h5>
+            </div>
+            <div class="col-xs-4">
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /simple stats with thumbnail -->
+    </div>
+
+
+    <div class="col-sm-6 col-lg-3">
+      <!-- Simple stats with thumbnail -->
+      <div class="panel">
+        <a href="#">
+          <img src="web/assets/images/mariadb.png" class="img-responsive" alt="">
+        </a>
+        <div class="panel-body">
+          <div class="row text-center">
+            <div class="col-xs-4">
+            </div>
+            <div class="col-xs-4">
+              <h5 class="text-semibold no-margin">MariaDB</h5>
+            </div>
+            <div class="col-xs-4">
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /simple stats with thumbnail -->
+    </div>
+
+    <div class="col-sm-6 col-lg-3">
+      <!-- Simple stats with thumbnail -->
+      <div class="panel">
+        <a href="#">
+          <img src="web/assets/images/apache.png" class="img-responsive" alt="">
+        </a>
+        <div class="panel-body">
+          <div class="row text-center">
+            <div class="col-xs-4">
+            </div>
+            <div class="col-xs-4">
+              <h5 class="text-semibold no-margin">Apache</h5>
+            </div>
+            <div class="col-xs-4">
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /simple stats with thumbnail -->
+    </div>
+
+    <div class="col-sm-6 col-lg-3">
+      <!-- Simple stats with thumbnail -->
+      <div class="panel">
+        <a href="#">
+          <img src="web/assets/images/bootstrap.png" class="img-responsive" alt="">
+        </a>
+        <div class="panel-body">
+          <div class="row text-center">
+            <div class="col-xs-4">
+            </div>
+            <div class="col-xs-4">
+              <h5 class="text-semibold no-margin">Bootstrap</h5>
+            </div>
+            <div class="col-xs-4">
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /simple stats with thumbnail -->
+    </div>
+
+    <div class="col-sm-6 col-lg-3">
+      <!-- Simple stats with thumbnail -->
+      <div class="panel">
+        <a href="#">
+          <img src="web/assets/images/jquery.png" class="img-responsive" alt="">
+        </a>
+        <div class="panel-body">
+          <div class="row text-center">
+            <div class="col-xs-4">
+            </div>
+            <div class="col-xs-4">
+              <h5 class="text-semibold no-margin">jQuery</h5>
+            </div>
+            <div class="col-xs-4">
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /simple stats with thumbnail -->
+    </div>
+
+    <div class="col-sm-6 col-lg-3">
+      <!-- Simple stats with thumbnail -->
+      <div class="panel">
+        <a href="#">
+          <img src="web/assets/images/atom.png" class="img-responsive" alt="">
+        </a>
+        <div class="panel-body">
+          <div class="row text-center">
+            <div class="col-xs-4">
+            </div>
+            <div class="col-xs-4">
+              <h5 class="text-semibold no-margin">Atom</h5>
+            </div>
+            <div class="col-xs-4">
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /simple stats with thumbnail -->
+    </div>
+
+    <div class="col-sm-6 col-lg-3">
+      <!-- Simple stats with thumbnail -->
+      <div class="panel">
+        <a href="#">
+          <img src="web/assets/images/phpmyadmin.png" class="img-responsive" alt="">
+        </a>
+        <div class="panel-body">
+          <div class="row text-center">
+            <div class="col-xs-4">
+            </div>
+            <div class="col-xs-4">
+              <h5 class="text-semibold no-margin">phpMyAdmin</h5>
+            </div>
+            <div class="col-xs-4">
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- /simple stats with thumbnail -->
+    </div>
+
+
+
+
+  </div>
+  <!-- /widgets list -->
